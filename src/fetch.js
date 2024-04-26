3409145d6ae4466b3328fd5ec6abcf715c30d30a
Veljko03 <veljkodovragovic@gmail.com>
2024-04-26 09:29:05 +0200

    added text fields for diplaying data from choosen city

---
+++ src/fetch.js
@@ -0,0 +1,39 @@
+export default async function fetchData() {
+  try {
+    const cityInput = document
+      .querySelector("#currLocation")
+      .value.toLowerCase();
+
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=f4c412f72ca6495f9e1114320242404&q=${cityInput}&aqi=no`,
+      { mode: "cors" }
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resources");
+    }
+    const data = await response.json();
+    const cityName = data.location.name;
+    const countryName = data.location.country;
+    const temperature = data.current.temp_c;
+    const fl = data.current.feelslike_c;
+    const wi = data.current.wind_kph;
+
+    console.log(data);
+    makeHtmlForNewCity(cityName, countryName, temperature, fl, wi);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function makeHtmlForNewCity(cityName, countryName, temperature, fl, wi) {
+  const displayCity = document.querySelector(".cityName");
+  const temp = document.querySelector(".temperature");
+  const feelsLike = document.querySelector(".feelsLike");
+  const wind = document.querySelector(".wind");
+
+  displayCity.textContent = `${cityName}, ${countryName}`;
+  temp.textContent = `${temperature}°C`;
+  feelsLike.textContent = `Feels like ${fl}°C`;
+  wind.textContent = "Wind: " + wi + "km/h";
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import "./style.css";
+import fetchData from "./fetch";
 
-console.log("hi here barrr");
+const btnConfirm = document.querySelector("#btnConfirm");
 
-async function fetchData() {
-  try {
-    const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=f4c412f72ca6495f9e1114320242404&q=London&aqi=no`,
-      { mode: "cors" }
-    );
-
-    if (!response.ok) {
-      throw new Error("Could not fetch resources");
-    }
-    const data = await response.json();
-  } catch (error) {
-    alert(error);
-  }
-}
+btnConfirm.addEventListener("click", () => {
+  fetchData();
+});
